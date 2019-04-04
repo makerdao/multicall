23b2e16add6f87f98e5eca4033907513e5907b02
Michael Elliot <michael@digital.com.au>
2019-04-04 14:53:45 +0800

    Add natural specification format comments

--- src/MulticallHelper.sol
+++ src/MulticallHelper.sol
@@ -1,5 +1,10 @@
 pragma solidity ^0.4.25;
 
+/// @title Multicall Helper Functions
+/// @notice These helper functions are provided in a separate contract because the main multicall contract can't call into itself
+/// @author Michael Elliot - <mike@makerdao.com>
+/// @author Joshua Levine - <joshua@makerdao.com>
+
 contract MulticallHelper {
     function ethBalanceOf(address addr) public view returns (uint256) {
         return addr.balance;
