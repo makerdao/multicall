24327de5a8d649f19884e4643ea5a33bd4263211
Michael Elliot <michael@digital.com.au>
2019-04-04 14:50:10 +0800

    Use solc 0.4.25

--- src/Multicall.sol
+++ src/Multicall.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.25;
 
 /// @title Multicall - Aggregate multiple constant function call results into one
 /// @author Michael Elliot - <mike@makerdao.com>
