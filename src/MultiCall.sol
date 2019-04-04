7abfa03b77cf203bc2d2ee71dbd81301d6dd2381
Michael Elliot <michael@digital.com.au>
2019-04-04 12:49:59 +0800

    Minor formatting changes

--- src/MultiCall.sol
+++ src/MultiCall.sol
@@ -1,22 +1,21 @@
 pragma solidity ^0.4.24;
 
-/// @title MultiCall - Aggregate multiple constant function calls into one
+/// @title Multicall - Aggregate multiple constant function call results into one
 /// @author Michael Elliot - <mike@makerdao.com>
 /// @author Joshua Levine - <joshua@makerdao.com>
 
 contract MultiCall {
-    function aggregate(bytes data) public view returns (bytes) {
+    function aggregate(bytes memory data) public view returns (bytes memory) {
         uint256 malloc;
         assembly { malloc := add(mul(mload(add(data, 0x20)), 0x20), 0x20) }
-        bytes memory tempBytes = new bytes(malloc);
+        bytes memory results = new bytes(malloc);
         uint256 _block = block.number;
         assembly {
-            mstore(add(tempBytes, 0x20), _block)
+            mstore(add(results, 0x20), _block)
             let ptr := mload(0x40)
             let cur := 0x40
             let inc := 2
             let len := mload(data)
-
             for { } lt(cur, len) { } {
                 let _target     := mload(add(data, cur))
                 let _retLen     := mul(mload(add(data, add(cur, 0x20))), 0x20)
@@ -24,12 +23,12 @@ contract MultiCall {
                 let _data       := add(data, add(cur, 0x80))
                 if eq(call(gas, _target, 0, _data, _dataLength, ptr, _retLen), 0)
                     { revert(0, 0) }
-                for { let offset := 0 } lt(offset, _retLen) { offset := add(offset, 0x20) } 
-                    { mstore(add(tempBytes, add(mul(inc, 0x20), offset)), mload(add(ptr, offset))) }
+                for { let offset := 0 } lt(offset, _retLen) { offset := add(offset, 0x20) }
+                    { mstore(add(results, add(mul(inc, 0x20), offset)), mload(add(ptr, offset))) }
                 inc := add(inc, mload(add(data, add(cur, 0x20))))
                 cur := add(cur, add(0x80, _dataLength))
             }
         }
-        return tempBytes;
+        return results;
     }
 }
